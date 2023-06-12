19f6a82cb8f8b82c31f772c58122d7cc7aa448aa
Rita Teixeira <rita.teixeira@codurance.com>
2023-06-12 16:11:28 +0200

    Correct failing tests

--- tests/acceptance/BirthdayGreeter.test.ts
+++ tests/acceptance/BirthdayGreeter.test.ts
@@ -1,6 +1,4 @@
 import {BirthdayGreetingService} from "../../src/BirthdayGreetingService";
-import {FriendsRepository} from "../../src/FriendsRepository";
-import {Friend} from "../../src/Friend";
 
 jest
   .useFakeTimers()
@@ -8,9 +6,7 @@ jest
 
 describe("Birthday Greeting", () => {
     it("should send a birthday greeting email to the friends that has their birthdays today", () => {
-        const friendsRepository: FriendsRepository = new FriendsRepository();
-        const birthdayFriends: Friend[] = friendsRepository.getBirthdayFriends();
-        const birthdayGreetingService: BirthdayGreetingService = new BirthdayGreetingService(birthdayFriends);
+        const birthdayGreetingService: BirthdayGreetingService = new BirthdayGreetingService();
 
         birthdayGreetingService.sendTodayGreetings();
         // fake notifier or test smtp
--- tests/friendsRepository.test.ts
+++ tests/friendsRepository.test.ts
@@ -1,4 +1,4 @@
-import {FriendsRepository} from "../src/FriendsRepository";
+import {friendsRepository} from "../src/FriendsRepository";
 import {Friend} from "../src/Friend";
 
 jest
@@ -7,7 +7,6 @@ jest
 
 describe("Friends repository", () => {
     it("get today's birthday friends", () => {
-        const friendsRepository = new FriendsRepository();
         const todaysFriendsBirthday: Friend[] = friendsRepository.getBirthdayFriends();
         const friend = new Friend("Doe", "John", "1982/10/08", "john.doe@foobar.com")
 
